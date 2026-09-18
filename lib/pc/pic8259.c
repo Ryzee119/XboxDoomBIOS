ee06fd1477982dfe2c763ecb17008a3cb2914440
Ryzee119 <wendland@live.com.au>
2026-09-19 09:03:50 +0930

    pic: Use pushf and cli to prevent ISR deadlock in IRQ mask

--- lib/pc/pic8259.c
+++ lib/pc/pic8259.c
@@ -1,27 +1,24 @@
 // SPDX-License-Identifier: CC0-1.0
 
-#include <stdatomic.h>
 #include <stdint.h>
 
-#include "cpu.h"
 #include "io.h"
-#include "lock.h"
 #include "pic8259.h"
-
-static atomic_flag lock;
 
 void pic8259_irq_enable(uint8_t data_port, uint8_t irq)
 {
-    spinlock_acquire(&lock);
+    uint32_t flags;
+    __asm volatile("pushf; pop %0; cli" : "=r"(flags));
     uint8_t mask = io_input_byte(data_port);
     io_output_byte(data_port, mask & (uint8_t) ~(1 << irq));
-    spinlock_release(&lock);
+    __asm volatile("push %0; popf" : : "r"(flags));
 }
 
 void pic8259_irq_disable(uint8_t data_port, uint8_t irq)
 {
-    spinlock_acquire(&lock);
+    uint32_t flags;
+    __asm volatile("pushf; pop %0; cli" : "=r"(flags));
     uint8_t mask = io_input_byte(data_port);
     io_output_byte(data_port, mask | (uint8_t)(1 << irq));
-    spinlock_release(&lock);
+    __asm volatile("push %0; popf" : : "r"(flags));
 }
